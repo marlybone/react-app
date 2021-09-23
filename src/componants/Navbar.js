a0574d06a2b7702f3eabf6bd0a6122de26483d3c
marlybone <marlybone@gmail.com>
2021-09-23 01:51:01 +0100

    navbar edits
    changed some css on the navbar to make it more streamlined and prettier. Tinkering with FA icons.

--- src/componants/Navbar.js
+++ src/componants/Navbar.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react' 
 import {Link} from 'react-router-dom'
-import { FaGalacticSenate } from "react-icons/fa";
-import { FaSun } from "react-icons/fa";
-import { FaBackward } from "react-icons/fa";
-import { FaAtom } from "react-icons/fa";
-import { FaAddressBook } from "react-icons/fa";
+import { faDiceD20, faStarAndCrescent, faVirus } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { faMeteor } from "@fortawesome/free-solid-svg-icons";
+import { faGem } from "@fortawesome/free-solid-svg-icons";
+import { faMinimize } from "@fortawesome/free-solid-svg-icons";
 import './Navbar.css';
-import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
@@ -25,41 +25,46 @@ function Navbar() {
     <ul className="navlist">
       <li className="nav-item">
           <Link to="/" className="navlink">
-            <FaBackward className='faimg' size='2.0em'/>
+            <FontAwesomeIcon className='icon' icon={faMinimize} size='2x'/>
+            <p className="link-text logo-text">Phonique</p>
             </Link>
-          <p className="link-text logo-text">Phonisssque</p>
+          
           
       </li>
       
       <li className="nav-item">
         <Link to="/" className="navlink">
-        <FontAwesomeIcon icon={faFire} size='2x' />
+        <FontAwesomeIcon icon={ faGem } size='2x' />
+        <span className="link-text">About</span>
             </Link>
-          <p className="link-text">About</p>
+          
         
       </li>
       
       <li className="nav-item">
         <Link to="/" className="navlink">
-            <FaAtom className='faimg' size='2.0em'/>
+        <FontAwesomeIcon icon={faMeteor} size='2x'/>
+        <p className="link-text">Portfolio</p>
             </Link>
-          <p className="link-text">Portfolio</p>
+          
         
       </li>
           
       <li className="nav-item">
         <Link to="/" className="navlink">
-            <FaAddressBook className='faimg' size='2.0em'/>
+        <FontAwesomeIcon icon={faDiceD20} size='2x'/>
+        <p className="link-text">Contact</p>
             </Link>
-          <p className="link-text">Contact</p>
+          
         
       </li>
           
         <li className="nav-item">
           <Link to="/" className="navlink">
-              <FaSun className='faimg' size='2.0em'/>
+          <FontAwesomeIcon icon={faSun} size='2x'/>
+          <p className="link-text">Themes</p>
               </Link>
-            <p className="link-text">Themes</p>
+            
           
       </li>
       </ul>
